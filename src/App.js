026dd8a6037574d64993038d7d9aa2e5b7d68b71
Sofia Ravelin <Sofia.Ravelin@hotmail.com>
2019-09-18 22:06:12 +0100

    Modifying

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Me from './components/me';
+import Reports from './reports/reports';
 import One from './reports/week/1';
+import Two from './reports/week/2';
+import Forms from './components/forms';
 import Error from './components/error';
 import Navigation from './components/navigation';
 
@@ -14,7 +17,11 @@ class App extends Component {
                     <Navigation />
                     <Switch>
                         <Route path="/" component={Me} exact/>
-                        <Route path="/reports/week/1" component={One}/>
+                        <Route path="/reports" component={Reports} />
+                            <Route path="/reports/week/1" component={One} />
+                            <Route path="/reports/week/2" component={Two} />
+                        <Route path="/forms" component={Forms} />
+                            <Forms />
                         <Route component={Error} exact/>
                         </Switch>
                 </div>
--- src/reports/week/1.js
+++ src/reports/week/1.js
@@ -5,12 +5,8 @@ import '../../style/1.css';
 const One = () => {
         return (
             <div className="week-page">
-            <h1>Redovisning</h1>
-            <p>Här finns redovisningtexter efter varje genomfört kursmoment.</p>
-            <li><a href="https://jsramverk.me/#repo">Kursrepo</a></li>
-            //<li><a href="../../../README.md">README.md</a></li>
             <h2>Week 1</h2>
-            <p>Week 1</p>
+            <p>Denna vecka har jag gjort grunden för denna sida i ReactJS. Jag har lagt mycket tid att försöka hitta en bra och tydlig struktur och att implementera ReactJS på ett korrekt sätt. Designen är väldigt enkel och kommer att utvecklas efter kursens gång.</p>
             </div>
         );
 };
