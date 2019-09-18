e07063c323cbe210ac54fc618acb4e825cfcecb6
Sofia Ravelin <Sofia.Ravelin@hotmail.com>
2019-09-18 22:01:05 +0100

    Trying to make a dropdown

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,8 +8,14 @@ const Navigation = () => {
         return (
         <nav>
             <ul className="nav-links">
-                <li><a href="/" path="/me">Home</a></li>
-                <li><a href="../reports/week/1">Reports</a></li>
+                <li><a href="/" path="/me">Hem</a></li>
+                <li><a href="/reports" path="./reports">Redovisningar</a>
+                    <ul>
+                        <li><a href="/reports/week/1" path="./reports/week/1">Vecka 1</a></li>
+                        <li><a href="/reports/week/2" path="./reports/week/2">Vecka 2</a></li>
+                    </ul>
+                </li>
+                <li><a href="./forms">Sign UP</a></li>
             </ul>
         </nav>
         );
